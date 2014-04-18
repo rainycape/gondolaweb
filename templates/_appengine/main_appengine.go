c651d703e8a06dcc14489a848e93c816014dd0c6
Alberto García Hierro <alberto@garciahierro.com>
2014-04-18 15:49:46 +0100

    Update GAE template
    Import the GCS blobstore driver by default.
    Configure the blobstore to use the default GCS bucket.

--- templates/_appengine/main_appengine.go
+++ templates/_appengine/main_appengine.go
@@ -6,8 +6,11 @@ import (
 	"net/http"
 	"time"
 
-	_ "gnd.la/admin" // required for make-assets command
-	_ "gnd.la/cache/driver/memcache"
+	_ "gnd.la/admin"                 // required for make-assets command
+	_ "gnd.la/cache/driver/memcache" // enable memcached cache driver
+	_ "gnd.la/orm/driver/gcs"        // enable Google Could Storage blobstore driver
+	// Uncomment the following line to use Google Cloud SQL
+	//_ "gnd.la/orm/driver/mysql"
 )
 
 func _app_engine_app_init() {
@@ -15,6 +18,9 @@ func _app_engine_app_init() {
 	// of this function runs.
 	for App == nil {
 		time.Sleep(5 * time.Millisecond)
+	}
+	if err := App.Prepare(); err != nil {
+		panic(err)
 	}
 	http.Handle("/", App)
 }
